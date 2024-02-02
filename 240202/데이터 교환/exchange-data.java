3d11cf906d81c0f9292dee71a9386fb62df2fc80
woorively <87566256+woorively@users.noreply.github.com>
2024-02-02 18:04:55 +0900

    Title: 데이터 교환, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240202/데이터 교환/exchange-data.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 6, c = 7;
+
+        int temp = a;
+        a = b;
+        b = temp;
+        temp = c;
+        c = a;
+        a = temp;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
