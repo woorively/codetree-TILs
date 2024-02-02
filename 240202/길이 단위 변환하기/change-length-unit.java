c8f83293ea251ab7d87fbcea0274103d1b90041f
woorively <87566256+woorively@users.noreply.github.com>
2024-02-02 17:37:27 +0900

    Title: 길이 단위 변환하기, Time: 99ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240202/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 9.2, b = 1.3;
+        System.out.printf("%.1fft = %.1fcm \n%.1fmi = %.1fcm", a, a*30.48, b, b*160934);
+    }
+}
