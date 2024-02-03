a87ca7d8ad44dc03b928bf2868c1003a8eadbf75
woorively <87566256+woorively@users.noreply.github.com>
2024-02-03 16:24:14 +0900

    Title: 그 계절, 그 날, Time: 130ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240203/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,24 @@
+y, m, d = tuple(map(int, input().split()))
+
+def leaf_year(y):
+    if y % 4:
+        return False
+    else:
+        if y % 100 == 0 and y % 400 == 0:
+            return True
+        elif y % 100 == 0 and y % 400 != 0:
+            return False
+        else:
+            return True
+
+if m == 2 and d == 29 and not leaf_year(y):
+    print(-1)
+
+if m in (12, 1, 2):
+    print("Winter")
+elif m in (3, 4, 5):
+    print("Spring")
+elif m in (6, 7, 8):
+    print("Summer")
+else:
+    print("Fall")
