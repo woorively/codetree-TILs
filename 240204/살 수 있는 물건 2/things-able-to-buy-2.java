2b0500c8a8ff01c8f034ad977ccdb5831ba2e84e
woorively <87566256+woorively@users.noreply.github.com>
2024-02-04 13:59:59 +0900

    Title: 살 수 있는 물건 2, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240204/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언
+        int n;
+        
+        // 입력
+        n = sc.nextInt();
+        
+        // 출력
+        if(n >= 3000)
+            System.out.println("book");
+        else if(n >= 1000)
+            System.out.println("mask");
+        else if(n >= 500)
+            System.out.println("pen");
+        else
+            System.out.println("no");
+    }
+}
