decfc117953234e69f3af79c0699e3988fa289f2
woorively <87566256+woorively@users.noreply.github.com>
2024-02-05 20:07:18 +0900

    Title: 공약수의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240205/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240205/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -1,13 +1,25 @@
-a,b = map(int,input().split())
-gong = []
-for i in range(2,1920):
-    if 1920 % i == 0 and 2880 % i ==0:
-        gong.append(i)
-sati = False
-for j in range(len(gong)):
-    if gong[j] >=a and gong[j] <=b:
-        sati = True
-if sati == True:
-    print(1)
-else:
-    print(0)
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 변수 선언 및 입력
+	    int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        boolean satisfied = false;
+
+        for(int i = a; i <= b; i++) {
+            // a에서 b사이의 값 중 공약수가 있는지 확인합니다.
+            if(1920 % i == 0 && 2880 % i == 0)
+                satisfied = true;
+        }
+
+        //출력
+        if(satisfied == true)
+            System.out.println("1");
+        else
+            System.out.println("0");
+    }
+}
