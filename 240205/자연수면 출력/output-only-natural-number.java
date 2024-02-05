3cc7e0e54479d284f5a287525a694d2940dbb2ec
woorively <87566256+woorively@users.noreply.github.com>
2024-02-05 15:56:19 +0900

    Title: 자연수면 출력, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240205/자연수면 출력/output-only-natural-number.java
+++ 240205/자연수면 출력/output-only-natural-number.java
@@ -13,6 +13,7 @@ public class Main {
             for (int i = 0; i < b; i++){
                 System.out.print(a);
             };
-        }
+        } else 
+            System.out.print(0);
     }
 }
