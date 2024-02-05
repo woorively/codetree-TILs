24dc84409619c709e2b42a4393fc476e7fd2bb80
woorively <87566256+woorively@users.noreply.github.com>
2024-02-05 18:02:25 +0900

    Title: 총합 구하기, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240205/총합 구하기/find-the-total.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, sum;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        sum = 0;
+
+        for (int i = a; i <= b; i++){
+            if (i % 6 == 0 && i % 8 != 0)
+                sum += i;
+        };
+        System.out.print(sum);
+    }
+}
