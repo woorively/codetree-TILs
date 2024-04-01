56eda3cd50499b9d0438cc7c01c8242092a25774
woorively <87566256+woorively@users.noreply.github.com>
2024-04-01 14:29:00 +0900

    Title: 줄 세우기 2, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

--- 240401/줄 세우기 2/line-up-students-2.py
+++ 240401/줄 세우기 2/line-up-students-2.py
@@ -1,11 +1,35 @@
 n = int(input())
 info = [
-    (input(), i + 1)
+    (tuple(map(int, input().split())), i + 1)
     for i in range(n)
 ]
 
-info.sort(key=lambda x: (x[0], -x[1]))
+info.sort(key=lambda x: (x[0][0], -x[0][1]))
 
 for elem in info:
     a, b = elem
-    print(a, b)
+    c, d = a
+    print(c, d, b)
+
+
+# class Member:
+#     def __init__(self, name, height, weight):
+#         self.name = name
+#         self.height = height
+#         self.weight = weight
+
+
+# n = int(input())
+# input_member = [
+#     tuple(input().split())
+#     for _ in range(n)
+# ]
+
+# members = [
+#     Member(name, int(height), int(weight))
+#     for name, height, weight in input_member
+# ]
+
+# members.sort(key=lambda x: (x.height, -x.weight))
+# for member in members:
+#     print(member.name, member.height, member.weight)
