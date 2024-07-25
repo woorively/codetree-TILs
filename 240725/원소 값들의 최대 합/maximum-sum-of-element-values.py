fb6b44d54486577f67b3ba1f5e1ad6a81508e81e
woorively <87566256+woorively@users.noreply.github.com>
2024-07-25 17:43:54 +0900

    Title: 원소 값들의 최대 합, Time: 157ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240725/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -0,0 +1,22 @@
+n, m = tuple(map(int, input().split()))
+nums = list(map(int, input().split()))
+
+# 5 1 4 2 3 -> 5
+# 3 1 4 2 5 -> 3
+# 4 1 3 2 5 -> 4
+# 2 1 3 4 5
+# 1 2 3 4 5
+
+ans = 0
+for num in nums:
+    # 앞에서부터 확인하면서
+    # 맨 앞 숫자를 원래 위치로 되돌려주기
+    total = num
+    # 현재 숫자를 temp 저장
+    temp = num
+    for _ in range(m-1):
+        total += nums[temp - 1]
+        temp = nums[nums.index(temp) - 1]
+
+    ans = max(ans, total)
+print(ans)
