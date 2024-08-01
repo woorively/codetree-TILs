2dbeb93052f895d7961ad1006e223a273e7b61ba
woorively <87566256+woorively@users.noreply.github.com>
2024-08-01 18:00:44 +0900

    Title: 그 전 알파벳, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240801/그 전 알파벳/before-the-alphabet.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        char alpha = sc.next().charAt(0);
+
+        System.out.print((char) (alpha-1));
+    }
+}
