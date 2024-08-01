487ea035d725a13e5653dc4edfc701234256663a
woorively <87566256+woorively@users.noreply.github.com>
2024-08-01 16:38:59 +0900

    Title: 두 번째를 첫 번째로, Time: 115ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240801/두 번째를 첫 번째로/second-to-first.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String ans = "";
+
+        for (int i=0; i<str.length(); i++){
+            if (str.charAt(i) == str.charAt(1)) {
+                ans += str.charAt(0);
+            } else {
+                ans += str.charAt(i);
+            }
+        }
+        
+        System.out.print(ans);
+    }
+}
