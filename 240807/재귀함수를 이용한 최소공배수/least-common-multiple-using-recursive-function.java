f953f3f9d70f3815201026bbfdae82f2601b71cd
woorively <87566256+woorively@users.noreply.github.com>
2024-08-07 14:56:10 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240807/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int ans = 0;
+
+        int[] arr = new int[n];
+        
+        int num1 = s.nextInt();
+        for (int i=0; i<n-1; i++){
+            int num2 = s.nextInt();
+            ans = lcm(num1, num2);
+            num1 = ans;
+        }
+
+        if (n == 1) {
+            ans = num1;
+        }
+
+        System.out.println(ans);
+    }
+
+    static int gcd(int a, int b){
+        if (a%b==0)
+            return b;
+
+        return gcd(b, a%b);
+    }
+
+    static int lcm(int a, int b){
+        return (a*b)/gcd(a, b);
+    }
+
+}
