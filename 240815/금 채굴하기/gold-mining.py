823dce85075b8e97c2c6d46a614f866a75ed17a9
woorively <87566256+woorively@users.noreply.github.com>
2024-08-15 11:26:11 +0900

    Title: 금 채굴하기, Time: 329ms, Memory: 27MB, Status: Passed - CodeTree

--- 240815/금 채굴하기/gold-mining.py
+++ 240815/금 채굴하기/gold-mining.py
@@ -4,12 +4,9 @@ map = [
     for _ in range(n)
 ]
 
-def in_range(r, c):
-    return 0 <= r < n and 0 <= c < n
-
 def get_gold(r, c, k):
     cnt = 0
-    profit = -(k * k - (k+1)*(k+1))
+    profit = -(k*k + (k+1)*(k+1))
 
     for i in range(n):
         for j in range(n):
@@ -17,16 +14,16 @@ def get_gold(r, c, k):
                 cnt += map[i][j]
 
     profit += cnt * m
+    
     return cnt if profit >= 0 else 0
 
 ans = 0
-max_cnt = 0
 for r in range(n):
     for c in range(n):
         for k in range(2 * n):
             cnt = get_gold(r, c, k)
 
-            if cnt > max_cnt:
+            if cnt > ans:
                 ans = cnt
 
 print(ans)
