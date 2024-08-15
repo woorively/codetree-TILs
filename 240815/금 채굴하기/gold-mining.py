a65f2c4c8223ee7a00d75411096a3974e323800e
woorively <87566256+woorively@users.noreply.github.com>
2024-08-15 11:15:23 +0900

    Title: 금 채굴하기, Time: 149ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240815/금 채굴하기/gold-mining.py
@@ -0,0 +1,32 @@
+n, m = tuple(map(int, input().split()))
+map = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+def in_range(r, c):
+    return 0 <= r < n and 0 <= c < n
+
+def get_gold(r, c, k):
+    cnt = 0
+    profit = -(k * k - (k+1)*(k+1))
+
+    for i in range(n):
+        for j in range(n):
+            if abs(r-i) + abs(c-j) <= k:
+                cnt += map[i][j]
+
+    profit += cnt * m
+    return cnt if profit >= 0 else 0
+
+ans = 0
+max_cnt = 0
+for r in range(n):
+    for c in range(n):
+        for k in range(n):
+            cnt = get_gold(r, c, k)
+
+            if cnt > max_cnt:
+                ans = cnt
+
+print(ans)
