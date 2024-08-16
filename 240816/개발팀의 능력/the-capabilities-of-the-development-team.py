e41713bc4dda28675497d831812018d94c5941aa
woorively <87566256+woorively@users.noreply.github.com>
2024-08-16 22:42:38 +0900

    Title: 개발팀의 능력, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240816/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 240816/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -2,8 +2,8 @@ nums = list(map(int, input().split()))
 nums.sort()
 
 t1 = nums[-1]
-t2 = nums[0] + nums[4]
-t3 = nums[2] + nums[3]
+t2 = nums[0] + nums[3]
+t3 = nums[1] + nums[2]
 
 ans = -1
 if (t1 != t2) and (t2 != t3) and (t3 != t1):
