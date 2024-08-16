3fba66e1237798ab96929a9076b3236723567e12
woorively <87566256+woorively@users.noreply.github.com>
2024-08-16 22:41:21 +0900

    Title: 개발팀의 능력, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240816/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,15 @@
+nums = list(map(int, input().split()))
+nums.sort()
+
+t1 = nums[-1]
+t2 = nums[0] + nums[4]
+t3 = nums[2] + nums[3]
+
+ans = -1
+if (t1 != t2) and (t2 != t3) and (t3 != t1):
+    maxT = max(t1, t2, t3)
+    minT = min(t1, t2, t3)
+    
+    ans = maxT - minT
+
+print(ans)
