cf94a74f6e04d586f1ada4081f457f9aee29d5b3
woorively <87566256+woorively@users.noreply.github.com>
2024-08-22 18:00:49 +0900

    Title: 계단 오르기, Time: 146ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240822/계단 오르기/climbing-stairs.py
@@ -0,0 +1,13 @@
+MAX_INT = 1000
+dp = [0] * (MAX_INT+1)
+
+n = int(input())
+dp[0] = 1
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
+
+for i in range(4, n+1):
+    dp[i] = (dp[i-1] + dp[i-3]) % 10007
+
+print(dp[n])
