93dd38665765f0dc9c453f04c238530a5a81d947
woorively <87566256+woorively@users.noreply.github.com>
2024-08-27 16:50:43 +0900

    Title: 사각형 채우기 3, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240827/사각형 채우기 3/rectangle-fill-3.java
+++ 240827/사각형 채우기 3/rectangle-fill-3.java
@@ -14,7 +14,7 @@ public class Main {
             if (i==2)
                 dp[2] = 7;
             else
-                dp[i] = (dp[i-1] * 3 + dp[i-2] - dp[i-3]) % 1000000007;
+                dp[i] = (dp[i-1] * 3 % 1000000007 + dp[i-2] - dp[i-3]) % 1000000007;
         }
 
         System.out.println(dp[n]);
