7041651130f9cac30b7a07e991da12c1e234c8ce
woorively <87566256+woorively@users.noreply.github.com>
2024-08-29 17:17:38 +0900

    Title: 외판원 순회, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240829/외판원 순회/traveling-salesman-problem.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static int n, minCost;
+    static int[] result;
+    static int[][] costMap;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        costMap = new int[n][n];
+        result = new int[n-1];
+        visited = new boolean[n];
+        minCost = Integer.MAX_VALUE;
+
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                costMap[i][j] = s.nextInt();
+            }
+        }
+        
+        huilai(0);
+        System.out.println(minCost);
+    }
+
+    static void huilai(int idx){
+        if (idx == n-1){
+        	minCost = Math.min(minCost, calc());
+//            System.out.println(Arrays.toString(result));
+            return;
+        }
+
+        for (int i=0; i<n-1; i++){
+            if (visited[i]) continue;
+
+            result[idx] = i;
+            visited[i] = true;
+            huilai(idx + 1);
+            visited[i] = false;
+        }
+    }
+    
+    static int calc() {
+        // 앞 뒤의 값
+        int cost = costMap[0][result[0]+1] + costMap[result[result.length-1]+1][0];
+
+        for (int i=0; i<result.length-1; i++){
+            cost += costMap[result[i]+1][result[i+1]+1];
+        }
+
+        return cost;
+    }
+}
