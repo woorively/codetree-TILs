10eeb6ac362106456418708ddd29ac781c0527d4
woorively <87566256+woorively@users.noreply.github.com>
2024-09-05 17:13:23 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 148ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240905/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+class Edge {
+    int x, y, z;
+
+    public Edge(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+public class Main {
+    static final int MAX_N = 5;
+    static int[][] graph = new int[MAX_N+1][MAX_N+1];
+    static boolean[] visited = new boolean[MAX_N+1];
+    static int[] dist = new int[MAX_N+1];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        // 주어진 간선 정보 (x, y, z)
+        // x -> y로 향하는 간선이 있으며, 가중치는 z 
+        Edge[] edges = new Edge[m+1];
+
+        for (int i=1; i<=m; i++) {
+            // int x = edges[i].x;
+            // int y = edges[i].y;
+            // int z = edges[i].z;
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+            graph[x][y] = z;
+            edges[i] = new Edge(x, y, z);
+        }
+
+        for (int i=1; i<=n; i++) {
+            dist[i] = Integer.MAX_VALUE;
+        }
+
+        dist[1] = 0;
+
+        for (int i=1; i<=n; i++) {
+            int minIdx = -1;
+            for (int j=1; j<=n; j++) {
+                if (visited[j]) continue;
+
+                if (minIdx == -1 || dist[minIdx] > dist[j])
+                    minIdx = j;
+            }
+
+            visited[minIdx] = true;
+            for (int j=1; j<=n; j++) {
+                if (graph[minIdx][j] == 0) continue;
+
+                dist[j] = Math.min(dist[j], dist[minIdx] + graph[minIdx][j]);
+            }
+        }
+
+        for (int i=2; i<=n; i++) {
+            if (!visited[i]) System.out.println(-1);
+            else System.out.println(dist[i]);
+        }
+
+    }
+    
+}
