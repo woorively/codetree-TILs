c4bc9007560faf526117bb3044ee65eced4b8cc4
woorively <87566256+woorively@users.noreply.github.com>
2024-09-08 09:26:10 +0900

    Title: 각 정점까지의 최단 경로, Time: 2284ms, Memory: 175MB, Status: Passed - CodeTree

--- 240908/각 정점까지의 최단 경로/shortest-path-to-each-vertex.java
+++ 240908/각 정점까지의 최단 경로/shortest-path-to-each-vertex.java
@@ -1,78 +1,89 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 
-public class Main {
-
-    static class Node {
-        int v, w; // 정점, 가중
-        
-        public Node(int v, int w) {
-            this.v = v;
-            this.w = w;
-        }
+class Node {
+    int index, dist;
+    
+    public Node(int index, int dist) {
+        this.index = index;
+        this.dist = dist;
     }
+};
+
+class Element implements Comparable<Element> {
+    int dist, index;
     
-    static int n, m;
-    static final int INF = Integer.MAX_VALUE;
-    static int[] weights;
-    static List<Node>[] graph;
+    public Element(int dist, int index) {
+        this.dist = dist;
+        this.index = index;
+    }
+
+    @Override
+    public int compareTo(Element e) {
+        return this.dist - e.dist; 
+    }
+};
+
+public class Main {    
+    public static final int MAX_N = 20000;
     
+    public static int n, m, k;
+    public static ArrayList<Node>[] graph = new ArrayList[MAX_N + 1];
+    public static PriorityQueue<Element> pq = new PriorityQueue<>();
+    
+    public static int[] dist = new int[MAX_N + 1];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         n = sc.nextInt();
         m = sc.nextInt();
-        
-        int k = sc.nextInt();
-        
-        graph = new ArrayList[n+1];
-        for (int i = 0; i <= n; i++) {
+        k = sc.nextInt();
+
+        for(int i = 1; i <= n; i++)
             graph[i] = new ArrayList<>();
+        
+        while(m-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+
+            graph[x].add(new Node(y, z));
+            graph[y].add(new Node(x, z));
         }
         
-        for (int i = 0; i < m; i++) {
-            int from = sc.nextInt();
-            int to = sc.nextInt(); 
-            int w = sc.nextInt();
-            
-            graph[from].add(new Node(to, w));
-        }
-        
-        weights = new int[n];
-        Arrays.fill(weights, INF); 
-        
-        dijkstra(k);
-        
-        for (int i = 0; i < n; i++) {
-            if (weights[i] == INF) {
-                System.out.println(-1);
-            } else {
-                System.out.println(weights[i]);
-            }
-        }
-        
-        sc.close();
-    }
-    
-    static void dijkstra(int start) {
-        weights[start] = 0; // 시작점의 가중치를 0으로 설정
-        boolean[] visited = new boolean[n];
-        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.w));
-        pq.add(new Node(start, 0));
-        
-        while (!pq.isEmpty()) {
-            Node curr = pq.poll();
-            
-            if (visited[curr.v]) continue;
-            visited[curr.v] = true;
-            
-            int w = weights[curr.v];
-            
-            for (Node child : graph[curr.v]) {
-                int nextW = w + child.w;
-                if (nextW < weights[child.v]) {
-                    weights[child.v] = nextW; 
-                    pq.add(new Node(child.v, nextW));
+        for(int i = 1; i <= n; i++)
+            dist[i] = (int)1e9;
+
+        dist[k] = 0;
+        pq.add(new Element(0, k));
+
+        while(!pq.isEmpty()) {
+            int minDist = pq.peek().dist;
+            int minIndex = pq.peek().index;
+            pq.poll();
+
+            if(minDist != dist[minIndex])
+                continue;
+
+            for(int j = 0; j < graph[minIndex].size(); j++) {
+                int targetIndex = graph[minIndex].get(j).index;
+                int targetDist = graph[minIndex].get(j).dist;
+                
+                int newDist = dist[minIndex] + targetDist;
+                if(dist[targetIndex] > newDist) {
+                    dist[targetIndex] = newDist;
+                    pq.add(new Element(newDist, targetIndex));
                 }
             }
+        }
+
+        for(int i = 1; i <= n; i++) {
+            if(dist[i] == (int)1e9)
+                System.out.println(-1);
+            else
+                System.out.println(dist[i]);
         }
     }
 }
