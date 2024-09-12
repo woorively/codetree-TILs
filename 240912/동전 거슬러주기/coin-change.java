4777a0fb967f8db51cc1f299d249400f341d441f
woorively <87566256+woorively@users.noreply.github.com>
2024-09-12 15:50:48 +0900

    Title: 동전 거슬러주기, Time: 192ms, Memory: 10MB, Status: Passed - CodeTree

--- 240912/동전 거슬러주기/coin-change.java
+++ 240912/동전 거슬러주기/coin-change.java
@@ -4,28 +4,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        int n = sc.nextInt(); // 동전의 수
+        int m = sc.nextInt(); // 금액
+
         int[] coins = new int[n];
-        int[] dp = new int[m+1];
-
-        Arrays.fill(dp, -1);
-
-        for (int i=1; i<=m; i++) {
-            int maxCnt = 0;
-
-            for (int j=0; j<coins.length; j++) {
-                int coin = coins[j];
-
-                if (i >= coin) {
-                    maxCnt = Math.max(maxCnt, dp[i-coin]+1);
-                }
-
-                dp[i] = maxCnt;
-            }
-            
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt(); // 동전의 값을 입력받음
         }
 
-        System.out.println(dp[n]);
+        int[] dp = new int[m + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE); // 최대값으로 초기화
+        dp[0] = 0; // 금액 0을 만드는 데 필요한 동전 수는 0
+
+        for (int i = 1; i <= m; i++) {
+            for (int coin : coins) {
+                if (i >= coin && dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        if (dp[m] == Integer.MAX_VALUE) {
+            System.out.println(-1); // 금액을 만들 수 없는 경우
+        } else {
+            System.out.println(dp[m]); // 최소 동전 수 출력
+        }
     }
 }
