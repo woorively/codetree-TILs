a091a9a2ce05442bade11fdedd681708f97292f3
woorively <87566256+woorively@users.noreply.github.com>
2024-09-12 15:41:43 +0900

    Title: 동전 거슬러주기, Time: 153ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240912/동전 거슬러주기/coin-change.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] coins = new int[n];
+        int[] dp = new int[m+1];
+
+        Arrays.fill(dp, -1);
+
+        for (int i=1; i<=m; i++) {
+            int maxCnt = 0;
+
+            for (int j=0; j<coins.length; j++) {
+                int coin = coins[j];
+
+                if (i > coin) {
+                    maxCnt = Math.max(maxCnt, dp[i-coin]+1);
+                }
+
+                dp[i] = maxCnt;
+            }
+            
+        }
+
+        System.out.println(dp[n]);
+    }
+}
