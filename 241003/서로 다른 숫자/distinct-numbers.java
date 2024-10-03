5392f912f41ed4fb5ce725488f4a3b1c758c11ce
woorively <87566256+woorively@users.noreply.github.com>
2024-10-03 12:43:33 +0900

    Title: 서로 다른 숫자, Time: 904ms, Memory: 39MB, Status: Passed - CodeTree

--- 241003/서로 다른 숫자/distinct-numbers.java
+++ 241003/서로 다른 숫자/distinct-numbers.java
@@ -1,16 +1,23 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.HashSet;
 
 public class Main {
+    public static final int MAX_N = 100000;
+    
+    public static int n;
+    public static int[] arr = new int[MAX_N];
+    public static HashSet<Integer> s = new HashSet<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
 
-        int n = sc.nextInt();
-        HashSet<Integer> hs = new HashSet<>();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
 
-        for (int i=0; i<n; i++) {
-            hs.add(sc.nextInt());
-        }
-
-        System.out.println(hs.size());
+        for(int i = 0; i < n; i++)
+            s.add(arr[i]);
+        
+        System.out.print(s.size());
     }
 }
