f7f361aae7128df43c9b5e22944b830b0f205c5f
woorively <87566256+woorively@users.noreply.github.com>
2024-10-05 19:30:14 +0900

    Title: 자리 차지하기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241005/자리 차지하기/take-place.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        TreeSet<Integer> ts = new TreeSet<>();
+        for (int i=1; i<=m; i++) {
+            ts.add(i);
+        }
+
+        int cnt = 0;
+        for (int i=0; i<n; i++) {
+            int num = sc.nextInt();
+
+            if (ts.lower(num) == null) {
+                break;
+            }
+
+            ts.remove(ts.floor(num));
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
