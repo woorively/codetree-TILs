5fdfd330ab5b1c1449eb4a0e04d423e74c4ce074
woorively <87566256+woorively@users.noreply.github.com>
2024-10-05 21:12:21 +0900

    Title: 작지만 큰 숫자, Time: 1833ms, Memory: 56MB, Status: Passed - CodeTree

---
+++ 241005/작지만 큰 숫자/small-but-big-number.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        TreeSet<Integer> ts = new TreeSet<>();
+
+        for (int i=0; i<n; i++) {
+            ts.add(sc.nextInt());
+        }
+
+        for (int i=0; i<m; i++) {
+            int num = sc.nextInt();
+
+            if (ts.floor(num) != null) {
+                System.out.println(ts.floor(num));
+                ts.remove(ts.floor(num));
+                continue;
+            }
+            System.out.println(-1);
+        }
+    }
+}
