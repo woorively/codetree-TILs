efef18329b957288f0c1e540f448bb30eac114fa
woorively <87566256+woorively@users.noreply.github.com>
2024-10-05 19:43:57 +0900

    Title: 차이가 가장 작은 수, Time: 1384ms, Memory: 48MB, Status: Passed - CodeTree

--- 241005/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
+++ 241005/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
@@ -1,32 +1,55 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Main {
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n, m;
+    public static int[] arr = new int[MAX_N];
+    
+    public static TreeSet<Integer> s = new TreeSet<>();
+    
+    // 답을 저장합니다.
+    public static int ans = INT_MAX;
+
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        // 입력으로 주어진 숫자를 전부 treeset에 넣어줍니다.
+        for(int i = 0; i < n; i++)
+            s.add(arr[i]);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        // 각 숫자 x 대해
+        // x보다 m 이상 더 크면서 가장 작은 값과
+        // x보다 m 이상 더 작으면서 가장 큰 값을 구해
+        // 차이가 가장 작은 경우를 갱신합니다.
+        for(int i = 0; i < n; i++) {
+            int x = arr[i];
 
-        TreeSet<Integer> ts = new TreeSet<>();
-        int minDiff = Integer.MAX_VALUE;
-        for (int i=0; i<n; i++) {
-            int num = sc.nextInt();
-            ts.add(num);
+            // x보다 m 이상 더 크면서 가장 작은 값은
+            // r - x >= m를 만족하는 최소 r이므로
+            // r >= m + x을 만족하는 최소 r을 구하면 됩니다.
+            if(s.ceiling(m + x) != null)
+                ans = Math.min(ans, s.ceiling(m + x) - x);
 
-            if (ts.ceiling(num+m) != null) {
-                minDiff = Math.min(minDiff, ts.ceiling(num+m) - num);
-            }
-            
-            if (ts.floor(num-m) != null) {
-                minDiff = Math.min(minDiff, num - ts.floor(num-m));
-            }
+            // x보다 m 이상 더 작으면서 가장 큰 값은
+            // x - r >= m를 만족하는 최대 r이므로
+            // r <= x - m을 만족하는 최대 r을 구하면 됩니다.
+            if(s.floor(x - m) != null)
+                ans = Math.min(ans, x - s.floor(x - m));
         }
 
-        if (minDiff == Integer.MAX_VALUE) {
-            System.out.println(-1);
-        } else {
-            System.out.println(minDiff);
-        }
+        // 불가능하다면 -1을 넣어줍니다.
+        if(ans == INT_MAX)
+            ans = -1;
+
+        System.out.print(ans);
     }
 }
