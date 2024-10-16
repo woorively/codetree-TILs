ec654d325b635f9fa66b5a4af37bd2cf6791cae1
woorively <87566256+woorively@users.noreply.github.com>
2024-10-16 19:59:58 +0900

    Title: G & H 반전시키기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241016/G & H 반전시키기/reversing-g-and-h.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String inputStr = sc.next();
+        String targetStr = sc.next();
+
+        int cnt = 1;
+        boolean isSame = false;
+
+        if (inputStr.charAt(0) == targetStr.charAt(0)) {
+            isSame = true;
+        }
+
+        for (int i=1; i<n; i++) {
+            if (inputStr.charAt(i) != targetStr.charAt(i) && isSame == true) {
+                isSame = false;
+                cnt++;
+            } else if (inputStr.charAt(i) == targetStr.charAt(i))
+                isSame = true;
+        }
+        
+        System.out.println(cnt);
+    }
+}
