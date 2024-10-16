19b66241ea6518e830291c2ffbaaecf7b5eb1139
woorively <87566256+woorively@users.noreply.github.com>
2024-10-16 20:19:02 +0900

    Title: 좌우 반전시키기, Time: 181ms, Memory: 10MB, Status: Passed - CodeTree

--- 241016/좌우 반전시키기/flip-left-and-right.java
+++ 241016/좌우 반전시키기/flip-left-and-right.java
@@ -6,17 +6,21 @@ public class Main {
 
         int n = sc.nextInt();
         int[] arr = new int[n];
-
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
         int ans = 0;
         for (int i=1; i<n; i++) {
+            // System.out.println(Arrays.toString(arr));
             if (arr[i-1] == 0) {
                 ans++;
                 for (int j=i-1; j<=i+1; j++) {
                     if (j==n) continue;
-                    arr[j] = arr[j] == 1? 0 : 1;
+                    arr[j] = (arr[j] == 1)? 0 : 1;
                 }
             }
-            // System.out.println(Arrays.toString(arr));
+            
         }
 
         if (arr[n-1] == 0) {
