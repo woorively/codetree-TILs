e39889f5985caa45db4d6a9c8fab84fb9ab44df1
woorively <87566256+woorively@users.noreply.github.com>
2024-10-19 22:09:44 +0900

    Title: 특별한 문자, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241019/특별한 문자/special-character.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
+            if (hm.containsKey(c) {
+
+            } else {
+                hm.put(c, 1);
+            }
+
+        }
+    }
+}
