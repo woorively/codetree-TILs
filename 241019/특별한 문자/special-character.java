fac6165e56e1bf069996b1751f7cfd01b3b967ac
woorively <87566256+woorively@users.noreply.github.com>
2024-10-19 22:24:10 +0900

    Title: 특별한 문자, Time: 478ms, Memory: 15MB, Status: Passed - CodeTree

--- 241019/특별한 문자/special-character.java
+++ 241019/특별한 문자/special-character.java
@@ -3,17 +3,31 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         String str = sc.next();
 
         HashMap<Character, Integer> hm = new HashMap<>();
-        for (int i=0; i<str.length(); i++){
-            char c = str.charAt(i);
-            if (hm.containsKey(c) {
+        List<Character> lst = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
 
+            if (hm.containsKey(ch)) {
+                // 이미 존재하는 문자
+                if (hm.get(ch) == 1) {
+                    lst.remove((Character) ch); // 캐스팅하여 Character 타입으로 제거
+                }
+                hm.put(ch, hm.get(ch) + 1);
             } else {
-                hm.put(c, 1);
+                // 새로운 문자
+                hm.put(ch, 1);
+                lst.add(ch);
             }
-
+        }
+        
+        if (lst.size() >= 1) {
+            System.out.println(lst.get(0));
+        } else {
+            System.out.println("None");
         }
     }
 }
