efd78d3b2d91e0ff8056ef696bd0e52880e039fd
woorively <87566256+woorively@users.noreply.github.com>
2024-10-22 12:53:56 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241022/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] dp = new int[n][n];
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                dp[i][j] = sc.nextInt();
+            }
+        }
+
+        int minMax = 0;
+        for (int i=1; i<n; i++) {
+            for (int j=1; j<n; j++) {
+                int min = Math.min(dp[i-1][j], dp[i][j-1]);
+                minMax = Math.max(minMax, min);
+            }
+        }
+
+        System.out.println(minMax);
+
+    }
+}
