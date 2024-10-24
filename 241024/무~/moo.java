a10e09d5b8e9c7e2db2c2ba76a94ec1216e383cd
woorively <87566256+woorively@users.noreply.github.com>
2024-10-24 17:23:38 +0900

    Title: 무~, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241024/무~/moo.java
+++ 241024/무~/moo.java
@@ -5,7 +5,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        Long n = sc.nextLong();
         if (n%3 == 1) {
             System.out.print("m");
         } else {
