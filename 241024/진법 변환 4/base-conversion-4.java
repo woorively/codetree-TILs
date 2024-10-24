099fbe1bdc9153df12d51dac5c1b9df14f61aaf7
woorively <87566256+woorively@users.noreply.github.com>
2024-10-24 17:19:17 +0900

    Title: 진법 변환 4, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241024/진법 변환 4/base-conversion-4.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String binaryNum = sc.next();
+        int ans = Integer.parseInt(binaryNum, 2);
+        System.out.println(ans);
+    }
+}
